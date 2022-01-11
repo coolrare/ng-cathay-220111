810b1b0d88c7efb49296338420448a269d30c136
Will <doggy.huang@gmail.com>
2022-01-11 17:15:20 +0800

    示範 Angular 元件生命週期

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Article } from './shared/Article';
 
 @Component({
@@ -6,16 +6,27 @@ import { Article } from './shared/Article';
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent implements OnInit, OnChanges, OnDestroy {
 
   @Input() item: Article = <Article>{};
 
   @Output() delete = new EventEmitter<number>();
 
   constructor() { }
+  ngOnDestroy(): void {
+    // throw new Error('Method not implemented.');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['item']) {
+      // changes['item'].currentValue
+    }
+  }
 
   ngOnInit(): void {
   }
+
+
 
   btnDelete() {
     this.delete.emit(this.item.id);
